7e15b6260476da5fdf60b1b714d8823f81982110
neodevelop <neodevelop@gmail.com>
2015-02-07 16:49:27 -0600

    Creando el Task Manager

---
+++ web/js/task_manager.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var TaskManager = (function() {
+
+  var create = function(task){
+  };
+
+  var read = function(task_id){
+  };
+
+  var update = function(task){
+  };
+
+  var eliminate = function(task){
+  };
+
+  var list = function(){
+  };
+
+  return {
+    create : create,
+    read : read,
+    update : update,
+    eliminate : eliminate,
+    list : list
+  };
+
+}());
