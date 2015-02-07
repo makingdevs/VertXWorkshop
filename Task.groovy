ec90cd38678c3b7326934a65112947673335a191
sohjiro <felipe@makingdevs.com>
2015-02-07 17:51:52 -0600

    agregando la función toMap para regresar un mapa de las propiedaes del task

--- Task.groovy
+++ Task.groovy
@@ -2,4 +2,8 @@ class Task {
   String uuid = UUID.randomUUID().toString().replace('-','')[0..15]
   String title
   String description
+
+  def toMap() {
+    [uuid: this.uuid, title:this.title, description:this.description]
+  }
 }
