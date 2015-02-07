852582057c95bc7ba35f909de31c13d869f22824
sohjiro <felipe@makingdevs.com>
2015-02-07 17:28:41 -0600

    imprimiendo mensaje del body para la obtención de datos

--- boardVerticle.groovy
+++ boardVerticle.groovy
@@ -2,10 +2,12 @@ def eb = vertx.eventBus
 
 eb.registerHandler("board.task.list") { message ->
   println "listing task"
+  message.reply "done"
 }
 
 eb.registerHandler("board.task.add") { message ->
   println "adding new task"
+  println message.body
 }
 
 eb.registerHandler("board.task.delete") { message ->
