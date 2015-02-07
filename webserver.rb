c3d4186cd4368db004ae9ae42ccf51d09431a4ef
sohjiro <felipe@makingdevs.com>
2015-02-07 11:58:32 -0600

    agregando llamadas para el sockjs y el vertxbus

--- webserver.rb
+++ webserver.rb
@@ -3,6 +3,10 @@ include Vertx
 
 server = HttpServer.new
 
-server.request_handler { |req| req.response.send_file("index.html") if req.uri == "/"}
+server.request_handler { |req|
+  req.response.send_file("index.html") if req.uri == "/"
+  req.response.send_file("bower_components/bower-sockjs-client/sockjs.js") if req.uri == "/sockjs.js"
+  req.response.send_file("bower_components/vertxbus/vertxbus.js") if req.uri == "/vertxbus.js"
+}
 
 server.listen(8080)
