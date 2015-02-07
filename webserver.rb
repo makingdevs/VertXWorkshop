22bb3cf192c46072d134579b856983d56a2acfd5
sohjiro <felipe@makingdevs.com>
2015-02-07 12:17:29 -0600

    #agregando el sockjs server y desplegando la vertical de las listas

--- webserver.rb
+++ webserver.rb
@@ -9,4 +9,8 @@ server.request_handler { |req|
   req.response.send_file("bower_components/vertxbus/vertxbus.js") if req.uri == "/vertxbus.js"
 }
 
+sockJSServer = SockJSServer.new(server)
+sockJSServer.bridge({'prefix' => '/eventbus'}, [], [])
 server.listen(8080)
+
+Vertx.deploy_verticle("list_verticle.groovy")
