95da286ec276e76f1f703de97ba2d81753d4f0e5
neodevelop <neodevelop@gmail.com>
2015-02-08 13:15:45 -0600

    Obteniendo data desde el task_manager

--- web/js/task_manager_editor.js
+++ web/js/task_manager_editor.js
@@ -6,6 +6,11 @@ TaskManagerEditor.prototype.findAll = function(){
   var self = this;
   this.eventBus.send("board.task.list", {}, function(message) {
     self.data = message;
+    console.log(message);
+    console.log(self.data);
   });
   return self.data;
+}
+TaskManagerEditor.prototype.getData = function(){
+  return this.data;
 }
