fa95cda2c33adfa7a2dd96549f1a32ed2b8594ab
neodevelop <neodevelop@gmail.com>
2015-02-08 13:12:53 -0600

    Intentando obtener la lista de tareas

--- web/js/task_manager_editor.js
+++ web/js/task_manager_editor.js
@@ -1,11 +1,11 @@
 function TaskManagerEditor(eventBus){
   this.eventBus = eventBus;
+  this.data = null
 }
 TaskManagerEditor.prototype.findAll = function(){
   var self = this;
-  var data = null
   this.eventBus.send("board.task.list", {}, function(message) {
-    data = message;
+    self.data = message;
   });
-  return data;
+  return self.data;
 }
