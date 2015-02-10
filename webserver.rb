1c52204845730dc90d64f9071f8e9373d68ca720
sohjiro <felipe@makingdevs.com>
2015-02-09 22:04:02 -0600

    agregando el persistor de mongo

--- webserver.rb
+++ webserver.rb
@@ -16,5 +16,16 @@ web_server_conf = {
   ]
 }
 
-Vertx.deploy_verticle("boardVerticle.groovy")
+mongo_persistor = {
+  "address" => "vertx.board",
+  "host" => "localhost",
+  "port" => 27017,
+  "pool_size" => 20,
+  "db_name" => "tasks",
+  "read_preference" => "nearest",
+  "use_mongo_types" => false
+}
+
 Vertx.deploy_module('io.vertx~mod-web-server~2.0.0-final', web_server_conf)
+Vertx.deploy_module('io.vertx~mod-mongo-persistor~2.1.0', mongo_persistor)
+Vertx.deploy_verticle("boardVerticle.groovy")
