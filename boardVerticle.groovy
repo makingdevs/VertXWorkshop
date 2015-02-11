8aaf5de8445cf0fc358059ae65950abf67937462
neodevelop <neodevelop@gmail.com>
2015-02-10 20:39:13 -0600

    Aplicando el borrado de tasks en MongoDB

--- boardVerticle.groovy
+++ boardVerticle.groovy
@@ -1,9 +1,4 @@
 def eventBus = vertx.eventBus
-def data = [
-  [uuid: UUID.randomUUID().toString().replace('-',''), title:"New title task",description:"New description task", status:'TODO'],
-  [uuid: UUID.randomUUID().toString().replace('-',''), title:"New title task 2",description:"New description task", status:'WIP'],
-  [uuid: UUID.randomUUID().toString().replace('-',''), title:"New title task 3",description:"New description task", status:'DONE']
-]
 
 eventBus.registerHandler("board.task.list") { message ->
   def query = [ action: "find", collection: "tasks" ]
@@ -16,16 +11,27 @@ eventBus.registerHandler("board.task.add") { message ->
   def query = [
     action: "save",
     collection: "tasks",
-    document: [uuid: UUID.randomUUID().toString().replace('-',''), title:message.body.title, description: message.body.description, status:'TODO']
+    document: [uuid: UUID.randomUUID().toString().replace('-',''),
+    title:message.body.title,
+    description: message.body.description,
+    status:'TODO']
   ]
-  eventBus.send('vertx.board', ) { messageBack ->
+  eventBus.send('vertx.board', query) { messageBack ->
     eventBus.publish("board.tasks.changed", null)
   }
 }
 
 eventBus.registerHandler("board.task.delete") { message ->
-  data.removeAll { t -> t.uuid == message.body.uuid }
-  eventBus.publish("board.tasks.changed", null)
+  def query = [
+    "action": "delete",
+    "collection": "tasks",
+    "matcher": [
+      "uuid": "${message.body.uuid}"
+    ]
+  ]
+  eventBus.send('vertx.board', query) { messageBack ->
+    eventBus.publish("board.tasks.changed", null)
+  }
 }
 
 eventBus.registerHandler("board.task.edit") { message ->
