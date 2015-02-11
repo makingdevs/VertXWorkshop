2d227c98105a35345ee5e605951b0c1b9b0824f5
neodevelop <neodevelop@gmail.com>
2015-02-10 23:03:39 -0600

    shared data no acepta mapas o hashes, hay que guardar un string y luego parsearlo

--- webserver.rb
+++ webserver.rb
@@ -1,34 +1,40 @@
 require "vertx"
+require 'json'
 
 #server = HttpServer.new
 #server.request_handler { |req| req.response.send_file("index.html") if req.uri == "/"}
 #server.listen(8080)
 
-#TODO: Usar sahred data para estos mapas de configuración
+shared_hash = Vertx::SharedData.get_hash('taskboard.config')
 
-web_server_conf = {
-  'port' => 8000,
-  'host' => 'localhost',
-  'bridge' => true,
-  "inbound_permitted" => [
-    { "address_re" => "board.task\\..+" }
-  ],
-  "outbound_permitted" => [
-    { "address" => "board.tasks.changed" }
-  ]
+puts shared_hash["web_server_conf"] => %{
+  {
+    'port' => 8000,
+    'host' => 'localhost',
+    'bridge' => true,
+    "inbound_permitted" => [
+      { "address_re" => "board.task\\..+" }
+    ],
+    "outbound_permitted" => [
+      { "address" => "board.tasks.changed" }
+    ]
+  }
 }
 
-mongo_persistor = {
-  "address" => "vertx.board",
-  "host" => "localhost",
-  "port" => 27017,
-  "pool_size" => 20,
-  "db_name" => "tasks",
-  "read_preference" => "nearest",
-  "use_mongo_types" => false
-}
+p shared_hash["web_server_conf"]
 
-Vertx.deploy_module('io.vertx~mod-web-server~2.0.0-final', web_server_conf)
-Vertx.deploy_module('io.vertx~mod-mongo-persistor~2.1.0', mongo_persistor)
-Vertx.deploy_verticle("manageVerticles.js")
-Vertx.deploy_verticle("boardVerticle.groovy")
+#shared_hash[:mongo_persistor] = {
+#  "address" => "vertx.board",
+#  "host" => "localhost",
+#  "port" => 27017,
+#  "pool_size" => 20,
+#  "db_name" => "tasks",
+#  "read_preference" => "nearest",
+#  "use_mongo_types" => false
+#}
+
+#my_hash = JSON.parse('{"hello": "goodbye"}')
+#Vertx.deploy_module('io.vertx~mod-web-server~2.0.0-final', shared_hash[:web_server_conf])
+#Vertx.deploy_module('io.vertx~mod-mongo-persistor~2.1.0', shared_hash[:mongo_persistor])
+#Vertx.deploy_verticle("manageVerticles.js")
+#Vertx.deploy_verticle("boardVerticle.groovy")
