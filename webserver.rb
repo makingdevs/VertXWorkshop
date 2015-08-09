06bf29d7f65edfbde2ff5318276e60c586e2cd1a
neodevelop <neodevelop@gmail.com>
2015-08-09 14:48:29 -0500

    Cambio el webserver de ruby para vertx3

--- webserver.rb
+++ webserver.rb
@@ -1,31 +1,42 @@
-require "vertx"
+require 'vertx-web/router'
+require 'vertx-web/static_handler'
+
+server = $vertx.create_http_server()
+
+router = VertxWeb::Router.router($vertx)
+
+router.route("/static/*").handler(&VertxWeb::StaticHandler.create().method(:handle))
+
+server.request_handler(&router.method(:accept)).listen(8080)
+
+#require "vertx"
 
 #server = HttpServer.new
 #server.request_handler { |req| req.response.send_file("index.html") if req.uri == "/"}
 #server.listen(8080)
 
-web_server_conf = {
-  'port' => 8000,
-  'host' => 'localhost',
-  'bridge' => true,
-  "inbound_permitted" => [
-    { "address_re" => "board.task\\..+" }
-  ],
-  "outbound_permitted" => [
-    { "address" => "board.tasks.changed" }
-  ]
-}
-
-mongo_persistor = {
-  "address" => "vertx.board",
-  "host" => "localhost",
-  "port" => 27017,
-  "pool_size" => 20,
-  "db_name" => "tasks",
-  "read_preference" => "nearest",
-  "use_mongo_types" => false
-}
-
-Vertx.deploy_module('io.vertx~mod-web-server~2.0.0-final', web_server_conf)
-Vertx.deploy_module('io.vertx~mod-mongo-persistor~2.1.0', mongo_persistor)
-Vertx.deploy_verticle("boardVerticle.groovy")
+#web_server_conf = {
+#  'port' => 8000,
+#  'host' => 'localhost',
+#  'bridge' => true,
+#  "inbound_permitted" => [
+#    { "address_re" => "board.task\\..+" }
+#  ],
+#  "outbound_permitted" => [
+#    { "address" => "board.tasks.changed" }
+#  ]
+#}
+#
+#mongo_persistor = {
+#  "address" => "vertx.board",
+#  "host" => "localhost",
+#  "port" => 27017,
+#  "pool_size" => 20,
+#  "db_name" => "tasks",
+#  "read_preference" => "nearest",
+#  "use_mongo_types" => false
+#}
+#
+#Vertx.deploy_module('io.vertx~mod-web-server~2.0.0-final', web_server_conf)
+#Vertx.deploy_module('io.vertx~mod-mongo-persistor~2.1.0', mongo_persistor)
+#Vertx.deploy_verticle("boardVerticle.groovy")
