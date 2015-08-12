c81b1bb281380160f324d7943ba8c5f015ec1b2e
neodevelop <neodevelop@gmail.com>
2015-08-11 21:38:33 -0500

    Agregando SockJS, su handler y las reglas de entrada/salida

--- webserver.rb
+++ webserver.rb
@@ -8,29 +8,24 @@ router = VertxWeb::Router.router($vertx)
 
 router.route("/static/*").handler(&VertxWeb::StaticHandler.create().method(:handle))
 
-sockJSHandler = VertxWeb::SockJSHandler.create($vertx)
-
 options = {
+  'inboundPermitteds' => [
+    { "addressRegex" => "board.task\\..+" }
+  ],
+  'outboundPermitteds' => [
+    { "address" => "board.tasks.changed" }
+  ]
 }
+
+sockJSHandler = VertxWeb::SockJSHandler.create($vertx, options)
+
 sockJSHandler.bridge(options)
 
 router.route("/eventbus/*").handler(&sockJSHandler.method(:handle))
 
-server.request_handler(&router.method(:accept)).listen(8080)
+server.request_handler(&router.method(:accept)).listen(8000)
 
 
-#web_server_conf = {
-#  'port' => 8000,
-#  'host' => 'localhost',
-#  'bridge' => true,
-#  "inbound_permitted" => [
-#    { "address_re" => "board.task\\..+" }
-#  ],
-#  "outbound_permitted" => [
-#    { "address" => "board.tasks.changed" }
-#  ]
-#}
-#
 mongo_persistor = {
   "host" => "localhost",
   "port" => 27017,
@@ -41,5 +36,5 @@ options = {
   "config" => mongo_persistor
 }
 
-#Vertx.deploy_module('io.vertx~mod-web-server~2.0.0-final', web_server_conf)
 $vertx.deploy_verticle("boardVerticle.groovy", options)
+
