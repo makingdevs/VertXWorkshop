8dfedb57740d1dcaa5a07939aa8ca0a4620e84e0
neodevelop <neodevelop@gmail.com>
2015-08-23 12:13:54 -0500

    Actualizando a VertX3 el administrador de verticles

--- manageVerticles.js
+++ manageVerticles.js
@@ -1,117 +1,90 @@
-var eventBus = require("vertx/event_bus");
-var console = require("vertx/console");
-var container = require('vertx/container');
-var vertx = require("vertx")
+var Vertx = require("vertx-js/vertx");
+var vertx = Vertx.vertx()
+var eventBus = vertx.eventBus();
 
-var config = container.config;
 var server = vertx.createNetServer();
-var verticles = { deploymentIds: [], webModuleId: null, mongoModuleId: null };
+var verticles = { deploymentIds: [] };
 
 server.connectHandler(function(socket) {
   console.log("A client has connected!");
 
-  vertx.fileSystem.readFile('makingdevs.txt', function(err, res){
-    socket.write(res);
+  vertx.fileSystem().readFile('makingdevs.txt', function(result, error){
+    socket.write(result);
   });
 
-  eventBus.registerHandler("write.to.socket",function(message){
-    socket.write(message + "\n")
+  eventBus.consumer("write.to.socket",function(message){
+    socket.write(message.body() + "\n")
   });
-  eventBus.registerHandler("verticle.groovy.deploy",function(message){
-    container.deployVerticle('boardVerticle.groovy',function(error, deploymentId){
-      if (!error) {
-        socket.write("The verticle has been deployed, deployment ID is " + deploymentId + "\n");
-        verticles.deploymentIds.push(deploymentId);
-      } else {
-        socket.write("Deployment failed! " + error.getMessage());
-      }
+
+  eventBus.consumer("verticle.deploy",function(message){
+    var filesToDeploy = splitString(message.body(), ',');
+    filesToDeploy.forEach(function(element, index, array){
+      var elementToDeploy = element.trim();
+      socket.write("Trying to deploy " + elementToDeploy + "\n");
+      vertx.deployVerticle(elementToDeploy,function(deploymentId, error){
+        if (!error) {
+          socket.write(elementToDeploy + " has been deployed, deployment ID is " + deploymentId + "\n");
+          verticles.deploymentIds.push(deploymentId);
+        } else {
+          socket.write("Deployment failed! " + error.getMessage());
+        }
+      });
     });
-    socket.write("boardVerticle.groovy deployed\n")
   });
-  eventBus.registerHandler("verticle.groovy.undeploy",function(message){
+
+  eventBus.consumer("verticle.undeploy",function(message){
     verticles.deploymentIds.forEach(function(element, index, array){
-      container.undeployVerticle(element);
+      vertx.undeploy(element);
       socket.write(element + " undeployed \n");
     });
     verticles.deploymentIds = [];
   });
-  eventBus.registerHandler("verticle.groovy.list",function(message){
+
+  eventBus.consumer("verticle.list",function(message){
     socket.write("Listing verticles \n")
     verticles.deploymentIds.forEach(function(element, index, array){
       socket.write(element + "\n");
     });
   });
-  eventBus.registerHandler("module.web.deploy",function(message){
-    socket.write("Deploying web module \n");
-    if(!verticles.webModuleId) {
-      container.deployModule("io.vertx~mod-web-server~2.0.0-final", config.web, function(err, deploymentId){
-        verticles.webModuleId = deploymentId;
-        socket.write("Deploying web module, done! \n");
-      });
-    } else {
-      socket.write("Already deployed a web module");
-    }
+
+  socket.handler(function(buffer){
+    executeCommand(buffer.toString("UTF-8"));
   });
-  eventBus.registerHandler("module.mongo.deploy",function(message){
-    socket.write("Deploying mongo module \n");
-    if(!verticles.mongoModuleId) {
-      container.deployModule("io.vertx~mod-mongo-persistor~2.1.0", config.mongo, function(err, deploymentId){
-        verticles.mongoModuleId = deploymentId;
-        socket.write("Deploying mongo module, done! \n");
-      });
-    } else {
-      socket.write("Already deployed a mongo module");
-    }
-  });
-  eventBus.registerHandler("module.web.undeploy",function(message){
-    if(verticles.webModuleId) {
-      socket.write("Undeploying web module \n");
-      container.undeployModule(verticles.webModuleId);
-      verticles.webModuleId = null;
-    } else {
-      socket.write("There is not web module instance\n");
-    }
-  });
-  eventBus.registerHandler("module.mongo.undeploy",function(message){
-    if(verticles.mongoModuleId) {
-      socket.write("Undeploying mongo module \n");
-      container.undeployModule(verticles.mongoModuleId);
-      verticles.mongoModuleId = null;
-    } else {
-      socket.write("There is not mongo module instance\n");
-    }
-  });
-  socket.dataHandler(function(buffer){
-    executeCommand(buffer.toString());
-  });
+
 });
 
 var executeCommand = function(command) {
+  var filesToApplyOperation = getVerticlesToApplyOperation(command)
+  var commandToApply = getCommandToApply(command)
   switch(true){
-    case /deploy/.test(command):
-      eventBus.send('verticle.groovy.deploy', command);
+    case /deploy/.test(commandToApply):
+      eventBus.send('verticle.deploy', filesToApplyOperation);
       break;
-    case /shutdown/.test(command):
-      eventBus.send('verticle.groovy.undeploy', command);
+    case /shutdown/.test(commandToApply):
+      eventBus.send('verticle.undeploy', filesToApplyOperation);
       break;
-    case /list/.test(command):
-      eventBus.send('verticle.groovy.list', command);
-      break;
-    case /web-module/.test(command):
-      eventBus.send('module.web.deploy', command);
-      break;
-    case /mongo-module/.test(command):
-      eventBus.send('module.mongo.deploy', command);
-      break;
-    case /close-web/.test(command):
-      eventBus.send('module.web.undeploy', command);
-      break;
-    case /close-mongo/.test(command):
-      eventBus.send('module.mongo.undeploy', command);
+    case /list/.test(commandToApply):
+      eventBus.send('verticle.list', filesToApplyOperation);
       break;
     default:
       eventBus.send('write.to.socket', "Command not found...");
   }
+}
+
+var getVerticlesToApplyOperation = function(command){
+  var separator = ' ';
+  var commandInTokens = splitString(command, separator);
+  var filesToDeploy = commandInTokens.slice(1).join(",");
+  return filesToDeploy;
+}
+
+var getCommandToApply = function(command){
+  return command.split(" ")[0];
+}
+
+function splitString(stringToSplit, separator) {
+  var arrayOfStrings = stringToSplit.split(separator);
+  return arrayOfStrings;
 }
 
 server.listen(1234, "localhost");
