5969566eb82fbe3fcd562fed3db5c2bb3dfb182b
neodevelop <juan@makingdevs.com>
2017-12-05 23:15:21 -0600

    Se usa `EventBus` en lugar de `vertx.EventBus`

--- demo2/webroot/js/app.js
+++ demo2/webroot/js/app.js
@@ -6,7 +6,9 @@ var TaskbordApp = (function() {
 
   var start = function(){
 
-    eventBus = new vertx.EventBus("http://localhost:8000/eventbus");
+    eventBus = new EventBus("http://localhost:8000/eventbus");
+    eventBus.enableReconnect(true);
+    console.log("Opening Eventbus...");
     eventBus.onopen = function() {
       console.log("The event bus is open");
       TaskManager.start(eventBus);
@@ -15,15 +17,15 @@ var TaskbordApp = (function() {
       });
       TaskFormController.start();
 
-      eventBus.registerHandler("board.tasks.changed", function(message) {
-        TaskManager.findAll(function(tasks){
-          TodoListView.render(tasks);
-          WipListView.render(tasks);
-          DoneListView.render(tasks);
-        });
-      })
+      //eventBus.registerHandler("board.tasks.changed", function(err, message) {
+      //  TaskManager.findAll(function(tasks){
+      //    TodoListView.render(tasks);
+      //    WipListView.render(tasks);
+      //    DoneListView.render(tasks);
+      //  });
+      //})
 
-      //eventBus.send("board.task.list", {}, function(message) {
+      //eventBus.send("board.task.list", {}, function(error, message) {
       //  console.log(message);
       //});
 
